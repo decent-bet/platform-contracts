6a98c95ea816917f4df5c76c32b19c4027c30dfe
dev <dev@decent.bet>
2018-04-15 19:54:27 +0400

    Update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -10,9 +10,7 @@ const mnemonic = process.env.MNEMONIC
 const infuraKey = process.env.INFURA_KEY
 const network = process.env.NETWORK
 
-const IS_DOCKER = process.env.NODE_ENV === 'docker'
-const HOST = IS_DOCKER ? 'ganache-cli' : 'localhost'
-const LOCAL_NODE_URL = 'http://' + HOST + ':8545'
+const LOCAL_NODE_URL = 'http://ganache-cli:8545'
 
 const networkUrl =
     network === NETWORK_DEVELOPMENT
@@ -38,7 +36,7 @@ module.exports = {
     migrations_directory: './migrations',
     networks: {
         development: {
-            host: HOST,
+            host: 'ganache-cli',
             port: 8545,
             network_id: '*', // Match any network id,
             from: provider.address,
